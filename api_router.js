08f5c737b7c1a8fb9903f914e67086eda3d6ef8e
jeremie-abt <jabt@student.42.fr>
2019-08-30 11:29:14 +0200

    correct error

--- api_router.js
+++ api_router.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const users_controller = require("./controller/users_controller")
-
+const images_controller = require("./controller/images_controller")
 let api_router = express.Router()
 
 // require of controller
@@ -12,6 +12,8 @@ let api_router = express.Router()
 api_router.get("/users/:user_id", users_controller.show)
 api_router.post("/users", users_controller.create)
 
+//images routes	
+api_router.get("/:user_id/images", images_controller.show)
 
 // pour l'instant c'est hardcoder
 module.exports = api_router
