c11326c2efc526c9db50b898237e85e709eaaa86
David LAURENT <dalauren@e1r13p4.42.fr>
2019-09-05 11:18:21 +0200

    fix(controller): return after error

--- server/controller/tags_controller.js
+++ server/controller/tags_controller.js
@@ -1,12 +1,12 @@
 const tags_model = require('../model/tags_model')
 
 function show(req, res) {
-  if(!req.params['tag_id']) {
-    res.status(400).send('A param is missing')
-    res.end()
-  }
   const tag_id = parseInt(req.params['tag_id'])
-
+  if(!tag_id || tag_id < 0) {
+    res.status(400).send('A param is missing or bad value or bad value')
+    res.end()
+    return
+  }
   tags_model.display_tag(tag_id)
     .catch(err => { throw err })
     .then(result => {
